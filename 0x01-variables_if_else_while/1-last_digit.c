2e455d2137656b4369cfebbc063800413feaa12d
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 01:53:06 -0800

    1-last_digit.c_file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("Last digit of %d is %d greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is  greater than 5\n", n, m);
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d less than 6 and not equal to zero \n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not equal to 0\n", n, m);
 	}
 	else if (m == 0)
 	{
-		prinf("Last digit of %d is %d and is equal to 0\n", n, m)
+		printf("Last digit of %d is %d and is equal to 0\n", n, m);
 	}
 	return (0);
 }
