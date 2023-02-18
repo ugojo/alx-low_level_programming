d7b5663d815e2d74de76b25a939e8314084999ac
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 15:56:38 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a all possible
+ * combination of signle-digit number
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+
+			if (a < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		putchar('\n');
+		return (0);
+	}
+
