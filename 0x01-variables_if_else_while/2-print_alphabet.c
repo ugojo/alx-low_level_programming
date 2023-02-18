ef15290a58010138aef2cbd47a0c2dfd70f1e2ef
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 02:15:45 -0800

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * main - A program that will assign a random number to the variable n each time it is executed
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ * main - A program that prints the alphabet in lowercase, followed by a new line.
  * Return: 0 (success)
  */
 
 int main(void)
 {
-	int n;
+	char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	if ((n % 10) > 5 )
+	c = 'a';
+
+	while (c <= 'z')
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", , n % 10);
+		putchar(c);
+			c++;
 	}
-	else if ((n % 10 ) < 6 && (n % 10 ) != 0 )
-	{
-		ptintf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
-	else 
-		printf("Last digit of %d is %d and is zero\n", n, n % 10);
-	return (0);
+	putchar('\n');
+		return (0)
 }
