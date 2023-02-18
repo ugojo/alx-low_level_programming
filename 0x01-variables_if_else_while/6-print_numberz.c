120b42db86cc3f09dad9fbe3a15b239988e06af0
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 05:57:56 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digit
+ * numbers of base 10 starting from 0
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	while ( a < 10)
+	{
+		putchar(a + '0');
+			a++;
+	}
+	putchar('\n');
+	return (0);
+}
