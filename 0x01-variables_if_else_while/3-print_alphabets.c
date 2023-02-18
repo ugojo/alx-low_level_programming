29063dc79d81bae67f3fe060cde64b2cee955dd2
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 02:45:35 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+	        d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
