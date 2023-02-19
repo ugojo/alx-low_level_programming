5c7f3dd233e9f2603c8380883dee2db5b0e94bee
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 18:07:16 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible
+ * different combinations of three digits.
+ * Return: 0 (success)
+ */
+
+init main(void)
+{
+	int n;
+
+
+	for (n = 12; n < 800; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+}
