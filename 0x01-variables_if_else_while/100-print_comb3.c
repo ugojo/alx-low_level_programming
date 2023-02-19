ba017bbb98b981d74107dcb9f112f5183be7ae0f
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-18 17:42:21 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 100; n++)
+	for (n = 1; n < 90; n++)
 	{
-		putchar((n/10) + '0');
-		putchar((n%10) + '0');
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
 	return (0);
 }
