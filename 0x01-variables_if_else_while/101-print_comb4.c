09c10edf55d794c21261939c42053e7148b24349
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-20 08:12:01 -0800

    101-print_comb4.c101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,29 @@
 int main(void)
 {
 	int n;
+	int m;
+	int l;
 
-
-	for (n = 2; n < 0; n++)
+	for (n = 48; n <= 58; n++)
 	{
-		putchar((n / 10) + '0');
-		putchar((n % 10) + '0');
-		putchar((n % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		for (m = 49; m <= 58; m++)
+		{
+			for (l = 50; l = 58; l++)
+			{
+				if (l > m || m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
 
+					if (n != 55 || m != 58)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
