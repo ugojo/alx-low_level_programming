47947f8995a3acc8514f8b253133bc523625b4fa
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-21 17:39:32 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include  <unistd.h>
 /**
- * _putchar - A program that prints _putchar,
- * followed by a new line
- * Return: 1 (success).
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ * Return: On 1 (success).
  * on error, -1 is returned, and erron is set approximately.
  */
 
