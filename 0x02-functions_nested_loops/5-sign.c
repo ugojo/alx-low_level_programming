b4261c105665953ae690ce63446a5a1cb15eb63b
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-21 20:30:59 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchart('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n = 0 )
 	{
-		_putchart('0');
+		_putchar('0');
 		return (0)
 	}
 	else
 	{
-		_putchart('-');
+		_putchar('-');
 		return (-1);
 	}
 }
