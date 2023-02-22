10c0221a8811f0c7a044cb84d0493c40cab76c93
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-21 19:35:24 -0800

    less than zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - A function that prints 
+ * the sign of a number.
+ * @n: Is the int to be checked
+ * Retrun: 1 and print + if n is greater than zero,
+ * 0 and print 0  if n is  is zero,
+ * otherwise -1 and print -
+ * if n less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+}
