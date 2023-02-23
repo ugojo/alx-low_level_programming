8121354f2c14ff247f142696bf4ec69cf860f9eb
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-23 00:26:42 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}else if (k >= 10)
+			}
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
