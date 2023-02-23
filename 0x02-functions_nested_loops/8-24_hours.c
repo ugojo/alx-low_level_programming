217b9ef8192e9ee1ebec59c13480ebf9c1f7ecec
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-02-23 00:15:57 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,9 +20,9 @@ void jack_bauer(void)
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar(j % 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++
+			j++;
 		}
 		i++;
 	}
