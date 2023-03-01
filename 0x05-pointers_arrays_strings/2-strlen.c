497ca0aa2c2edd708dacba3dbd4876d58a6c44ed
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-01 05:55:23 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - A function that returns
+ * the length of a string.
+ *
+ * Return: The value of the string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
