4954d2c5f8e47e50ce20be5aabc0c4ff0be477a7
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-02 17:23:46 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int t;
 
-	for (i = (n - 1); i >= 0; i--)
+	for (i = 0; i < n--; i++)
 	{
-		
-		a[i]
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
