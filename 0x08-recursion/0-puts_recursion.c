ccc4bc95866e3fd98f7610022ac1b69eca737954
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-08 13:59:38 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
