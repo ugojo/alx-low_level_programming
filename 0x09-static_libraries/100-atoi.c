532588851602e9fb21c8f781aa14895cb74f9bc3
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-10 04:42:47 -0800

    0x09-static_libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,19 +1,27 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
  */
-char *_strchr(char *s, char c)
+int _atoi(char *s)
 {
-	int i = 0;
-
-
-	for (; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+	int sign = 1, i = 0;
+	unsigned int res = 0;
+	
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+ 	{
+ 		if (s[i] == '-')
+ 			sign *= -1;
+ 		i++;
+ 	}
+ 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+ 	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+ 	}
+ 	res *= sign;
+ 	return (res);
 }
