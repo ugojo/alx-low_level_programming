7273bb079e119ad7935ed82f5f6027bd4b9ae51b
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-11 04:39:17 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 /**
  * main - program that multiplies two numbers
  * @argc: number of arguments
@@ -8,7 +8,7 @@
  */
 
 
-int main(argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2, mul;
 
@@ -21,8 +21,9 @@ int main(argc, char *argv[])
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-
 		mul = num1 * num2;
+
+		printf("%d\n", mul);
 	}
-	return (mul);
+	return (0);
 }
