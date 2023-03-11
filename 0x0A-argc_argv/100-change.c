f44f188f5ab57731a9a3a153f2590a3c58d132d3
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-11 06:06:15 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
 
-	if (agrc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	cents = atoi(agrv[1]);
+	cents = atoi(argv[1]);
 
 	while (cents > 0)
 	{
