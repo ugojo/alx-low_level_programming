fd6e9f32c8f7e5e2a334a0e93ff33662b5c743f0
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-16 08:35:01 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,13 +16,13 @@ int *array_range(int min, int max)
 
 	size = max - min + 1;
 	ptr = malloc(sizeof(int) * size);
-		
+
 	if (ptr == NULL)
-		
+
 		return (NULL);
 
 	for (i = 0; min <= max; i++)
 
 		ptr[i] = min++;
-	return (ptr);		
+	return (ptr);
 }
