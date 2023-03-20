d425b3daa1809a03d32d16965d5b3aa573378638
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-20 08:20:44 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function-like macro ABS(x) that computes
+ * the absolute value of a number x.
+ * @ABS(): the macro function
+ * @x: the value to compute
+ */
+#ifndef ABS(x)
+#define ABS(x) (x)
+
+int a;
+
+#if x <= 0
+a = (-1) * x;
+#endif
+#if x > 0
+a = (x)
+#endif
