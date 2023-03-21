9ce5d406bb8815aae83c1d1be26b5310929807d3
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-21 10:59:52 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nill)";
+	}
+	if (d->age == NULL)
+	{
+		d->age = "(nill)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nill)"
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
