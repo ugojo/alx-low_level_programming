9672d4015487bd27dc6bc92b9f87ed9ecd099b55
obi ugochukwu joseph <obiugochukwu003@gmail.com>
2023-03-21 15:55:47 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,69 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	len1 = strlen(src);
+
+	for (int i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest = "\0";
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	*_strcpy(dog->name, name);
+	*_strcpy(dog->owner, owner);
+
+	dog->age = dog;
+	return (dog);
+}
