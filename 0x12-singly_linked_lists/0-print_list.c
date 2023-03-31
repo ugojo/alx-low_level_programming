85efe24f86bde7594eec5b0fcb9e1cc65a677a36
obi ugochukwu <obiugochukwu003@gmail.com>
2023-03-30 18:05:39 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t c = 0;
 
 	while (h)
 	{
@@ -18,9 +18,9 @@ size_t print_list(const list_t *h)
 		else
 			 printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
-	count++;
+	c++;
 	}
 
-	return (count);
+	return (c);
 }
 
