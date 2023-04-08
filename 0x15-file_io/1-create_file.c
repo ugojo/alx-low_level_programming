bcd7efd302016ec886707116eb68a9b050ac0dab
obi ugochukwu <obiugochukwu003@gmail.com>
2023-04-08 05:36:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,21 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
 	len = strlen(text_content);
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fp == -1)
+	{
+		return (-1);
+	}
 
 	w = write(fp, text_content, len);
-	if (fp == -1 || w == -1)
+	if (w == -1)
 	{
 		return (-1);
 	}
