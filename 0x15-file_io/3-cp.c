c0084f9ebb3304657d94530f8abd9926c977294d
obi ugochukwu <obiugochukwu003@gmail.com>
2023-04-08 15:13:16 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,83 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_handler - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+
+void error_handler(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	int close_file;
+	ssize_t r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(agrv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_handler(file_from, file_to, argv);
+
+	r = 1024;
+	while (r == 1024)
+	{
+		r = read(file_from, buffer, 1024);
+		if (r  == -1)
+		{
+			error_handler(-1, 0, argv);
+		}
+		w = write(file_to, buffer, r);
+		if (w == -1)
+		{
+			error_handler(0, -1, argv);
+		}
+	}
+	close_file = close(file_from);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	close_file = close(file_to);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,3 +1,4 @@
 1-create_file.c
 1-create_file.c
 2-append_text_to_file.c
+0x15-file_io
