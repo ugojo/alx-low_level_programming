a422f9e262aa986d9c1f44201df0fd5c00252354
obi ugochukwu <obiugochukwu003@gmail.com>
2023-04-09 06:50:17 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file_from = open(agrv[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_handler(file_from, file_to, argv);
 
