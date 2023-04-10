c196efd3311c3ee2bff0c039933e18f74a610ffb
obi ugochukwu <obiugochukwu003@gmail.com>
2023-04-09 17:16:23 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int  i, c;
+	c = 0;
+	unsigned long int dec;
+
+	for (i = 64; i < 0; i--)
+	{
+		dec = n >> 1;
+		if (dec & 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c)
+		{
+			_putchar('0');
+		}
+	}
+	if (!c)
+	{
+		_putchar('0');
+	}
+}
